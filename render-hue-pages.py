1ab9fc0c941bb3c92a4fab1794149071e00f0528
Shawn Becker <sbecker11@me.com>
2023-07-22 21:59:22 -0600

    added column 0 grey values

--- render-hue-pages.py
+++ render-hue-pages.py
@@ -5,23 +5,37 @@ from PIL import Image, ImageDraw
 width = 75
 height = 75
 gap = 10
-max_rows = 9
-max_cols = 19
+max_rows = 11
+max_cols = 20
 
-def create_hue_page(page_hue_name, cl_df, huepages_df):
-    selected_rows = cl_df.loc[cl_df['Page_HueName'] == page_hue_name, ['Value_Row', 'Chroma_Column', 'Chip_RGB']]
+def create_hue_page(page_hue_name, cl_df, gr_df, huepages_df):
+    # selected_rows = cl_df.loc[cl_df['Page_HueName'] == page_hue_name, ['Value_Row', 'Chroma_Column', 'Chip_RGB', 'Chip_Color_Key']]
+    selected_rows = cl_df.loc[cl_df['Page_HueName'] == page_hue_name]
     rows_data = []
+    rows = [item[1] for item in selected_rows.iterrows()]
+    rows.append(gr_df.loc[0])
+    rows.append(gr_df.loc[10])
+    for row in rows:
+        value_row = int(row['Value_Row'])
+        col_suffixes =['','_x'] if (value_row > 0 and value_row < 10) else ['']
+        # rgb_counter = {}
+        for suffix in col_suffixes:
+            chroma_column = row['Chroma_Column'+suffix]  # Use .iloc[0] to get the first (and only) value as an integer
+            row_idx = value_row - 1
+            col_idx = chroma_column // 2
+            x1 = gap // 2 + (col_idx + 1 - 1) * (width + gap)
+            y1 = gap // 2 + (max_rows - row_idx - 1 - 1) * (height + gap)
+            x2 = x1 + width
+            y2 = y1 + height
+            rgb = str(row['Chip_RGB'+suffix])
+            # if rgb not in rgb_counter:
+            #     rgb_counter[rgb] = 0
+            # rgb_counter[rgb] += 1
+            r, g, b = map(int, rgb.split(','))
+            key = str(row['Chip_Color_Key'+suffix])
+            rows_data.append((x1, y1, x2, y2, (r, g, b), key))
 
-    for _, row in selected_rows.iterrows():
-        value_row = int(row['Value_Row']) - 1
-        chroma_column = int(row['Chroma_Column']) // 2 - 1
-        x1 = gap // 2 + chroma_column * (width + gap)
-        y1 = gap // 2 + (max_rows - value_row - 1) * (height + gap)
-        x2 = x1 + width
-        y2 = y1 + height
-        r, g, b = map(int, row['Chip_RGB'].split(','))
-        rows_data.append((x1, y1, x2, y2, (r, g, b)))
-
+        # print("value_row:", value_row, "rgb_counter:", rgb_counter);
     image_file = huepages_df.loc[huepages_df.index == page_hue_name, 'Page_Image_File'].values[0]
     return rows_data, image_file
 
@@ -32,10 +46,39 @@ def main():
     # Read the "Conversion Lists" sheet into a DataFrame
     xl = pd.ExcelFile(excel_file_path)
     cl_df = xl.parse("Conversion Lists")
-    
+
+    # Calculate the before_cols before the merge
+    before_cols = sorted([str(c) for c in cl_df.columns])
+    before_length = len(before_cols)
+    before_set = set(before_cols)
+
     # Read the "HuePages" sheet into a DataFrame
     huepages_df = xl.parse("HuePages")
     huepages_df.set_index('Page_HueName', inplace=True)
+
+    # Read the "Gray" sheet into a separate DataFrame
+    gr_df = xl.parse("Gray")
+
+    # Merge gr_df with cl_df using a left join on "Value_Row"
+    cl_df = cl_df.merge(gr_df, on="Value_Row", how="left", suffixes=('', '_x'))
+
+    # identical to rename(columns)
+    # cols_x_map = [(cx,cx.replace('_x','')) for cx in cl_df.columns if cx.endswith('_x')]
+    # for cx in cols_x_map:
+    #     cl_df[cx[0]] = cl_df[cx[1]]
+
+    # Rename col_x and col_y to col after the merge
+    # cl_df.rename(columns={col + '_x': col for col in before_set}, inplace=True)
+
+    # Calculate the after_cols after the merge and renaming
+    # after_cols = sorted([str(c) for c in cl_df.columns])
+    # after_length = len(after_cols)
+    # after_set = set(after_cols)
+
+    # # Check if column names are altered after the merge and renaming
+    # if not before_cols == after_cols:
+    #     print(f"Error: Column names before:{before_length} after:{after_length} in cl_df have been altered after the merge and rename.")
+    #     exit(1)
 
     # Create the image folder if it doesn't exist
     if not os.path.exists(image_folder):
@@ -46,7 +89,7 @@ def main():
 
     # Process each page_hue_name and create the images
     for page_hue_name in page_hue_names:
-        rows_data, image_file = create_hue_page(page_hue_name, cl_df, huepages_df)
+        rows_data, image_file = create_hue_page(page_hue_name, cl_df, gr_df, huepages_df)
 
         # Create a 720x800 pixel image with transparent background
         image_height = gap // 2 + max_rows * (height + gap)
@@ -55,7 +98,7 @@ def main():
         draw = ImageDraw.Draw(image)
 
         # Draw the filled rectangles on the image
-        for x1, y1, x2, y2, color in rows_data:
+        for x1, y1, x2, y2, color, key in rows_data:
             draw.rectangle([x1, y1, x2, y2], fill=color + (255,))  # Set alpha channel to 255 (opaque)
 
         # Save the image to a PNG file
