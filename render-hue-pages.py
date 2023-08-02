cbd8fcfa53a7a0aca7bd2880fc39e2d3b0cd3369
Shawn Becker <sbecker11@me.com>
2023-08-01 18:12:50 -0600

    removed greys

--- render-hue-pages.py
+++ render-hue-pages.py
@@ -10,34 +10,26 @@ gap = 10
 max_rows = 11
 max_cols = 20
 
-def create_hue_page(page_hue_name, cl_df, gr_df, huepages_df):
+def create_hue_page(page_hue_name, cl_df, huepages_df):
     # selected_rows = cl_df.loc[cl_df['Page_HueName'] == page_hue_name, ['Value_Row', 'Chroma_Column', 'Chip_RGB', 'Chip_Color_Key']]
     selected_rows = cl_df.loc[cl_df['Page_HueName'] == page_hue_name]
     rows_data = []
     rows = [item[1] for item in selected_rows.iterrows()]
-    rows.append(gr_df.loc[0])
-    rows.append(gr_df.loc[10])
     for row in rows:
         value_row = int(row['Value_Row'])
-        col_suffixes =['','_x'] if (value_row > 0 and value_row < 10) else ['']
-        # rgb_counter = {}
-        for suffix in col_suffixes:
-            chroma_column = row['Chroma_Column'+suffix]  # Use .iloc[0] to get the first (and only) value as an integer
-            row_idx = value_row - 1
-            col_idx = chroma_column // 2
-            x1 = gap // 2 + (col_idx + 1 - 1) * (width + gap)
-            y1 = gap // 2 + (max_rows - row_idx - 1 - 1) * (height + gap)
-            x2 = x1 + width
-            y2 = y1 + height
-            rgb = str(row['Chip_RGB'+suffix])
-            # if rgb not in rgb_counter:
-            #     rgb_counter[rgb] = 0
-            # rgb_counter[rgb] += 1
-            r, g, b = map(int, rgb.split(','))
-            color_key = str(row['Chip_Color_Key'+suffix])
-            rows_data.append((x1, y1, x2, y2, (r, g, b), color_key, chroma_column, value_row))
+        chroma_column = row['Chroma_Column']  # Use .iloc[0] to get the first (and only) value as an integer
+        row_idx = value_row - 1
+        col_idx = chroma_column // 2
+        x1 = gap // 2 + (col_idx + 1 - 1) * (width + gap)
+        y1 = gap // 2 + (max_rows - row_idx - 1 - 1) * (height + gap) # use max_rows to invert the y values
+        x2 = x1 + width
+        y2 = y1 + height
+        rgb = str(row['Chip_RGB'])
+        r, g, b = map(int, rgb.split(','))
+        color_key = str(row['Chip_Color_Key'])
+        rows_data.append((x1, y1, x2, y2, (r, g, b), color_key, chroma_column, value_row))
 
-        # print("value_row:", value_row, "rgb_counter:", rgb_counter);
+    # print("value_row:", value_row, "rgb_counter:", rgb_counter);
     image_file = huepages_df.loc[huepages_df.index == page_hue_name, 'Page_Image_File'].values[0]
     return rows_data, image_file
 
@@ -61,30 +53,6 @@ def main():
     huepages_df = xl.parse("HuePages")
     huepages_df.set_index('Page_HueName', inplace=True)
 
-    # Read the "Gray" sheet into a separate DataFrame
-    gr_df = xl.parse("Gray")
-
-    # Merge gr_df with cl_df using a left join on "Value_Row"
-    cl_df = cl_df.merge(gr_df, on="Value_Row", how="left", suffixes=('', '_x'))
-
-    # identical to rename(columns)
-    # cols_x_map = [(cx,cx.replace('_x','')) for cx in cl_df.columns if cx.endswith('_x')]
-    # for cx in cols_x_map:
-    #     cl_df[cx[0]] = cl_df[cx[1]]
-
-    # Rename col_x and col_y to col after the merge
-    # cl_df.rename(columns={col + '_x': col for col in before_set}, inplace=True)
-
-    # Calculate the after_cols after the merge and renaming
-    # after_cols = sorted([str(c) for c in cl_df.columns])
-    # after_length = len(after_cols)
-    # after_set = set(after_cols)
-
-    # # Check if column names are altered after the merge and renaming
-    # if not before_cols == after_cols:
-    #     print(f"Error: Column names before:{before_length} after:{after_length} in cl_df have been altered after the merge and rename.")
-    #     exit(1)
-
     # Create the image folder if it doesn't exist
     if not os.path.exists(image_folder):
         os.makedirs(image_folder)
@@ -95,7 +63,7 @@ def main():
     colors = []
     # Process each page_hue_name and create the images
     for page_hue_name in page_hue_names:
-        rows_data, image_file = create_hue_page(page_hue_name, cl_df, gr_df, huepages_df)
+        rows_data, image_file = create_hue_page(page_hue_name, cl_df, huepages_df)
 
         # Create a 720x800 pixel image with transparent background
         image_height = gap // 2 + max_rows * (height + gap)
@@ -132,3 +100,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print("done")
